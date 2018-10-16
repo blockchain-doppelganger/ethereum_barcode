bf86e0bab373debe0d46624e157a40173fc34762
blockchain-doppelganger <doppengalger@tutanota.com>
2018-10-16 19:34:07 +0300

    add some comments, add search fiels

--- src/js/kabinet/product.js
+++ src/js/kabinet/product.js
@@ -1,8 +1,10 @@
 
 function initWeb3(){
 
+    // import module to below function, return hash of string
     const createKeccakHash = require('keccak')
 
+    // to get address in correct format 
     function toChecksumAddress(address) {
         address = address.toLowerCase().replace('0x', '')
         var hash = createKeccakHash('keccak256').update(address).digest('hex')
@@ -23,45 +25,44 @@ function initWeb3(){
     const devery = require('@devery/devery');
     const DeveryRegistry = devery.DeveryRegistry;
     const utils = devery.Utils;
+    let deveryRegistryClient = new DeveryRegistry();
+    // add to global variable
     window.utils = utils;
-    let deveryRegistryClient = new DeveryRegistry({address:'0xf3838287aeae92300bd98a4d82ad5b841ff35f58'});
-    // let deveryRegistryClient = new DeveryRegistry();
     window.deveryRegistryClient = deveryRegistryClient;
 
    
 	if (typeof web3 !== 'undefined') {
 	    web3 = new Web3(web3.currentProvider);
-		console.log('done ...')
+		// console.log('done ...')
 		// $('.container').show();
         $('.warning').html();
         const addressUser = web3.eth.coinbase;
         deveryRegistryClient.getApp(addressUser).then(app => {
-            // console.log(app)
-            // console.log(app.appAccount);
-            // console.log(utils.isAddress(app.appAccount));
+            // check if we right connect to ethereum via metamask
             const emptyAddress = /^0x0+$/.test(app.appAccount);
             if (emptyAddress){
                 $('#product_warning').html("You didn't register your company and brand. Please go to Brand link and follow the instructions");
             }
             else {
+                //ok, we're connected, no error ...
                 $('#product_warning').html('');
+                // get user address in correct format
                 const userAddress = toChecksumAddress(web3.eth.coinbase);
-                // console.log(userAddress)
-                var lengthProduct =0;
+                var lengthProduct = 0;
+
+                // get all user product and display they on product page
                 deveryRegistryClient.productAccountsLength().then(function(res){
-                    console.log(res);
                     for(let i = 0;i< res;i++){
                          deveryRegistryClient.productAccountsArray(i).then(address => {
-                            deveryRegistryClient.getProduct(address).then(product => {
-                                // console.log(product.brandAccount);
+                            deveryRegistryClient.getProduct(address).then(product => { 
                                  if (product.brandAccount == userAddress) {
                                     lengthProduct++;
-                                    console.log(address);
-                                    // lass=c"btn btn-primary" 
-                                    //aria-expanded="false
+                                    
+                                    // create button to toggle product information
                                     $('</p>').append(
                                         $( "<a/>", {
                                             html: address,
+                                            "style":'color:white',
                                             "class":"btn btn-primary ",
                                             href: "."+address,
                                             "data-toggle":"collapse",
@@ -70,12 +71,13 @@ function initWeb3(){
                                             "aria-controls":address
                                         })).
                                     appendTo('.productList');
-
+                                    
+                                    // all product info will be in form input, so user can update info
                                     var newform = $('<form></form>',{
                                         "class":address + ' collapse '
                                     });
 
-
+                                    // product description fiels
                                     var descField = $('<div></div>',{
                                        "class":"form-group col-md-6"
                                     });
@@ -87,8 +89,8 @@ function initWeb3(){
                                         "required":true,
                                        "placeholder": product.description
                                      }).appendTo(descField);
-                                //    $('<hr/>').appendTo('.productList');
 
+                                     // product details fiels
                                    var detailsField = $('<div></div>',{
                                         "class":"form-group col-md-6"
                                     });
@@ -133,18 +135,22 @@ function initWeb3(){
                                 detailsField.appendTo(newform);
                                 yearField.appendTo(newform);
                                 originField.appendTo(newform);
+
+                                // create button to update product info
                                 $('<a/>',{
                                     "role":'button',
                                     "class":"btn btn-info",
                                     html:'update info',
                                     "click":function(e){
-                                        e.preventDefault;
-                                        let desc = $('.'+address +'  #updateProductDesc').val();
-                                        let det = $('.'+address +'  #updateProductDet').val();
-                                        let year = $('.'+address +'  #updateProductYear').val();
-                                        let orig = $('.'+address +'  #updateProductOrigin').val();
+
+                                        const prodAddress = $(this).parent().attr('class').split(' ')[0];
+                                        const desc = $(this).parent().find('#updateProductDesc').val();
+                                        const det = $(this).parent().find('#updateProductDet').val();
+                                        const year = $(this).parent().find('#updateProductYear').val();
+                                        const orig = $(this).parent().find('#updateProductOrigin').val();
+                                        const acti = $(this).parent().find('#updateProductActive').is(':checked');
                                         
-                                        deveryRegistryClient.updateProduct(address,desc,det,year,orig,true).then(transaction => {
+                                        deveryRegistryClient.updateProduct(prodAddress,desc,det,year,orig,true).then(transaction => {
                                             // console.log('transaction address',transaction.hash);
                                             //... other stuff
                                         }).catch(err => {
@@ -159,26 +165,19 @@ function initWeb3(){
                                 }).appendTo(newform)
                                 $('<hr>').appendTo(newform);
                                 newform.appendTo('.productList');
-                                // $('<br>').appendTo('.productList');
                                 
+                                //
                                 $('#userProductCount').html(lengthProduct);
                                 }
                                  else{
-                                    //  console.log('none');
+                                    
                                  }
-                                //  console.log(brand.brandName);
-                                  //... do more stuff
+                                
                              })
                           })
                      }
-
-                    //  console.log(lengthBrand);
                 });  
             }
-            // if(app.active){
-            //     console.log(app.appName);
-            //     //... other stuff
-            // }
         })
 	} else {
 	  // Set the provider you want from Web3.providers
@@ -186,9 +185,6 @@ function initWeb3(){
 		// $('.container').hide();
 	}
 
-	// var networkId = web3.version.network;
-	// var user_address = web3.eth.accounts[0];
-	// console.log(user_address);
 
 }
 
